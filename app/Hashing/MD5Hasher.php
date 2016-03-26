cff3b3f2768abc0f4a69543d8a7981c4bef5d24c
srph <seraphipod@gmail.com>
2016-03-27 07:23:30 +0800

    Add MD5Hasher

---
+++ app/Hashing/MD5Hasher.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Hashing;
+
+use RuntimeException;
+use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+
+class MD5Hasher implements HasherContract
+{
+
+    /**
+     * Hash the given value.
+     *
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    public function make($value, array $options = [])
+    {
+        return md5($value);
+    }
+
+    /**
+     * Check the given plain value against a hash.
+     *
+     * @param  string  $value
+     * @param  string  $hashedValue
+     * @param  array   $options
+     * @return bool
+     */
+    public function check($value, $hashedValue, array $options = [])
+    {
+        return $this->make($value) === $hashedValue;
+    }
+
+    /**
+     * Check if the given hash has been hashed using the given options.
+     *
+     * @param  string  $hashedValue
+     * @param  array   $options
+     * @return bool
+     */
+    public function needsRehash($hashedValue, array $options = [])
+    {
+        return false; // Don't really need it with MD5
+    }
+}
---
+++ app/Providers/HashServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Hashing\MD5Hasher;
+
+class HashServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('hash', function () {
+            return new MD5Hasher;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['hash'];
+    }
+}
