82a9f964fc2c3530c125cae7e0ba520217333da7
srph <seraphipod@gmail.com>
2016-03-27 19:09:53 +0800

    Fix missing `code` parameter

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,7 +2,7 @@ Greetings, <br>
 <br>
 Welcome to Valiant ROSE, <strong>{{ $username }}</strong>! We're excited to meet you in-game. However, to proceed, you may need to verify your email. Here's the link to your <strong><a href="{{ route('register.confirmation', ['code' => $code ]) }}">email confirmation</a></strong>.<br>
 <br>
-If that doesn't work, you please paste this link to your address bar instead -> <strong><a href="{{ route('register.confirmation') }}">{{ route('register.confirmation', ['code' => $code ]) }}</a></strong>.<br>
+If that doesn't work, you please paste this link to your address bar instead -> <strong><a href="{{ route('register.confirmation', ['code' => $code]) }}">{{ route('register.confirmation', ['code' => $code ]) }}</a></strong>.<br>
 <br>
 <strong>Happy Playing</strong>!<br>
 <br>
